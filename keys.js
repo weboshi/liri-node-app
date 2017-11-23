515f90e322c13463742f796b1fa6ced0b0fec684
weboshi <avident@gmail.com>
2017-11-22 17:03:11 -0800

    huzzah

--- keys.js
+++ keys.js
@@ -1,15 +1,19 @@
-JavaScript
+
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
+  consumer_key: '6fssJNkBBMInn7tk5b10Fz4Gv',
+  consumer_secret: '90LKI0L6IqtnlVWlEcwBWZDxfAubGC7e1Sfx6OQux4Mf1ck9ME',
+  access_token_key: '174188650-3RSNZjkfNDy21hZU8iz12jt9QruSgnEvLrW8gaox',
+  access_token_secret: 'TBMWgO1aEeT9Y5TnfwCwoRGuVaKzdmgFXWH8wOHlxfSyv',
+};
+
+var spotify = {
+  id: '232c1cf757fa4a6a97ef3647e8be1d43',
+  secret: '7dae693bbb3f4ce89d2ac8fc6a2af08c'
+};
 
 module.exports = {
     twitterKeys: twitterKeys,
-    spotifyKeys: spotifyKeys,
-    
+    spotify: spotify
 }
--- liri.js
+++ liri.js
@@ -1,26 +1,67 @@
 
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
-var twitterKey = keys.twitterKeys
-var spotifyKey = keys.spotifyKeys
 
-var myTweets = function() {
-    var client = new twitter(dataKeys.twitterKeys);
-  
-    var params = { screen_name: 'autasho', count: 20 };
-  
+if (process.argv[2] == "my-tweets") {
+
+  var client = new Twitter(keys.twitterKeys);
+ 
+    var params = {screen_name: 'autasho', count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  
       if (!error) {
-        var data = [];
-        for (var i = 0; i < tweets.length; i++) {
-          data.push({
-              'created at: ' : tweets[i].created_at,
-              'Tweets: ' : tweets[i].text,
-          });
+        for(var i = 0 ; i < tweets.length; i++) {
+          console.log(tweets[i].text);
+          console.log(tweets[i].created_at);
         }
-        console.log(data);
-        writeToLog(data);
       }
-    });
-  };
+      else {
+        console.log(oops);
+      }
+  })
+}
+
+    
+if (process.argv[2] == "spotify-this-song") {
+  songname = process.argv[3]
+
+
+  var spotify = new Spotify(keys.spotify)
+
+  spotify.search({ type: 'track', query: songname, limit: '1'}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  else {
+    console.log(data.items)
+  }
+
+
+})
+};
+
+if (process.argv[2] == "movie-this") {
+  var request = require('request');
+  var movieName = process.argv[3]
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+  
+  
+  request(queryUrl, function(error, response, body) {
+  
+
+    if (!error && response.statusCode === 200) {
+  
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IDMB Rating: " + JSON.parse(body).imdbRating)
+      console.log("Rotten Rating: " + JSON.parse(body).Ratings)
+      console.log("Made in: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+    }
+})
+}
